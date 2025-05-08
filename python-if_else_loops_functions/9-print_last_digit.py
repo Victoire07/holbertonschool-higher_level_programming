c922a0b6ef23e514d6be410dfe491d244c139332
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 00:42:30 +0400

    Affiche et retourne dernier chiffre d1 nbre

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
+    valeur_absolue = abs(number)
+    dernier_chiffre = valeur_absolue % 10
+    print("{}".format(dernier_chiffre), end="")
+    return dernier_chiffre
