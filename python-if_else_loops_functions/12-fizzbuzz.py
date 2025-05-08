9160b4942fee1798a316da0b499bd27ac6e4f050
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 01:18:26 +0400

    Ajout condition pour les autres nombres ni mult de 3 ou 5 ou les 2

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,3 +3,9 @@ def fizzbuzz():
     for nombre in range (1, 101):
         if nombre % 3 == 0 and nombre % 5 == 0:
             print("FizzBuzz", end =" ")
+        elif nombre % 3 == 0:
+            print("Fizz", end=" ")
+        elif nombre % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(nombre, end=" ")
