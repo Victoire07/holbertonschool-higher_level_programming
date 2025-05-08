89f9a1eb830daabe6f6fc0265279ec5b2c505cc6
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 01:20:41 +0400

    Modif pour pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for nombre in range (1, 101):
+    for nombre in range(1, 101):
         if nombre % 3 == 0 and nombre % 5 == 0:
-            print("FizzBuzz", end =" ")
+            print("FizzBuzz", end=" ")
         elif nombre % 3 == 0:
             print("Fizz", end=" ")
         elif nombre % 5 == 0:
