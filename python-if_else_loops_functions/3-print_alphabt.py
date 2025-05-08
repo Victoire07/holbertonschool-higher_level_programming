5e7742baf285f480c153e0aa44a7430dca12ad2b
Victoire07 <victoireboutin07@gmail.com>
2025-05-08 15:25:40 +0400

    Modification code pour ne pas afficher lettre e & lettre q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print("{}".format("".join(
-    chr(lettre) for lettre in range(97, 123)
-    if lettre != 101 and lettre != 113
-)))
+for i in range(97, 123):
+    if (i != 101 and i != 113):
+        print("{}".format(chr(i)), end='')
