a50fb44888ba83b0d1d457c271373b1e88b636ee
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 00:05:49 +0400

    Ajout fonction permettant de transormer en maj

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+
 def uppercase(str):
+    resultat = ""  # initialisation chaîne vide au début
+
+    for caractere in str:
+        if ord(caractere) >= 97 and ord(caractere) <= 122:
+            majuscule = chr(ord(caractere) - 32)
+        else:
+            majuscule = caractere
+
+        resultat += majuscule
+
+    print("{}".format(resultat))
+
