27140eda58cf20f53c32dbdd8af2ee88c3d0b809
Victoire07 <victoireboutin07@gmail.com>
2025-05-08 15:19:47 +0400

    Modif code avec asci pour respect consigne

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print("{}".format("".join(
-    lettre for lettre in "abcdefghijklmnopqrstuvwxyz"
-    if lettre != "e" and lettre != "q"
+    chr(lettre) for lettre in range(97, 123)
+    if lettre != 101 and lettre != 113
 )))
