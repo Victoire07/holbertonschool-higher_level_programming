c868eaca07af707d02b8aeaad13c3b0bc696ef41
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 11:57:00 +0400

    Modif pycodestyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     total_args = 0
 for nombre in range(1, len(sys.argv)):
-    total += int(sys.argv[nombre])
-    print(total)
+    total_args += int(sys.argv[nombre])
+    print(total_args)
