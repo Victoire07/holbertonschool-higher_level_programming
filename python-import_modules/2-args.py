4d095068237185f7486b86a2e7508bf2fe197dd4
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 10:29:46 +0400

    Ajout coeur du corps pour compter nb args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+  import sys
+  nb_args = len(sys.argv) - 1
+
+if nb_args == 0:
+    print("0 arguments.")
+elif nb_args == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(nb_args))
+
+
