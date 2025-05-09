232de1c3bfda76780a732200f5c5b04bd6b50024
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 10:40:31 +0400

    ajout code pour gestion si ≠ O arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import sys
-   nb_args = len(sys.argv) - 1
-   
-   if nb_args == 0:
-      print("0 arguments.")
+    import sys
+    nb_args = len(sys.argv) - 1
+
+    if nb_args == 0:
+        print("0 arguments.")
     elif nb_args == 1:
-       print("1 argument:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(nb_args))
+        for nombre in range (1, len(sys.argv)):
+            print("{}: {}".format(nombre, sys.argv[nombre]))
