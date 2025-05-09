ccac55e64a3775571e3a83ab1c5e74e9c38bdb62
Victoire07 <victoireboutin07@gmail.com>
2025-05-09 10:41:34 +0400

    Modif pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(nb_args))
-        for nombre in range (1, len(sys.argv)):
+        for nombre in range(1, len(sys.argv)):
             print("{}: {}".format(nombre, sys.argv[nombre]))
