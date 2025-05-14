503674075179952e09e4c11f6c628b63c415194f
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 17:24:39 +0200

    Ajout des conditions pour rmeplacer un element si necessaire ou pas

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    nouvelle_list = []
+    for element in my_list:
+        if element == search:
+            nouvelle_list.append(replace)
+        else:
+            nouvelle_list.append(element)
+    return (nouvelle_list)
