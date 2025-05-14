b1fb8717be450179ac049ff2703777a102542564
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 14:52:22 +0200

    Modification pour passation de la norme pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     longueur = len(sentence)
-    if(len(sentence) == 0):
+    if (len(sentence) == 0):
         premier = None
     else:
         premier = sentence[0]
-    return(longueur, premier)
-
+    return (longueur, premier)
