6fd96844badd308a0f007954e7773b0b51625ee5
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 15:58:10 +0200

    Fonction permettant de savoir si nmbre divisable par 2 ou pas

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    resultat = []
+    for nombre in my_list:
+        if nombre % 2 == 0:
+            resultat.append(True)
+        else:
+            resultat.append(False)
+    return (resultat)
+
