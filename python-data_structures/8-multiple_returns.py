67ec6e36a310375cd78b2232bef040744c44353c
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 14:47:39 +0200

    conditions

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    longueur = len (sentence)
+    longueur = len(sentence)
+    if(len(sentence) == 0):
+        premier = "None"
+    else:
+        premier = sentence[0]
+    return()
+
