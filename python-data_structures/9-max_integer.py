2387ea7a7879992880045cde57d93713035a1dba
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 15:18:51 +0200

    Fonction qui retourne le + gd nombre

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_valeur = my_list[0]
+
+    for nombre in my_list[1:]:
+        if nombre > max_valeur:
+            max_valeur = nombre
+
+    return max_valeur
