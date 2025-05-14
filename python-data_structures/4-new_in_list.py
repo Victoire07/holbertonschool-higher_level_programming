600e8e9d4b4d0583050d94d4036035933cab6fc1
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 10:18:33 +0200

    Fonction permettant de modifier une liste sans modif originale

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= len(my_list):
-        return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
     else:
-        return() 
+        copie = my_list[:]
+        copie [idx] = element
+        return copie
