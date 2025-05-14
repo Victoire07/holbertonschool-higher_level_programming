df6722ac0a5340ade9e576e4b1626d4f0f6f255f
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 17:07:26 +0200

    modification map car syntaxe incrorect

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for ligne in matrix:
-
+        ligne_au_carree = map(lambda x: x**2, ligne)
+        new_matrix.append(ligne_au_carree)
+        return(new_matrix)
