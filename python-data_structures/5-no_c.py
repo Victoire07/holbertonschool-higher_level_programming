3b270950c063af5b5d364528467600507abfb347
Victoire07 <victoireboutin07@gmail.com>
2025-05-14 10:37:46 +0200

    Modif fonction dans boucle cdtion if

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_str = ""
+    for i in (my_string):
+        if i != 'c' and i != 'C':
+            return(new_str += i)
+        else:
+            return(new_str)
