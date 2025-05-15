0b96cb869f88772e68365087ecd84c910f7f8731
Victoire07 <victoireboutin07@gmail.com>
2025-05-15 23:10:30 +0200

    Deuxième version modification des 2 variables crées et return de fin

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,3 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
+    score_max = 0
+    nom_associe = ""
+    for key, value in a_dictionary.items():
+        return (nom_associe)
+
