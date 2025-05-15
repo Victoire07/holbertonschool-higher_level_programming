a84df4733831c916a48188cc2da7ecf4831d5ebc
Victoire07 <victoireboutin07@gmail.com>
2025-05-15 23:50:25 +0200

    ajout de 2 variables, lecture de la chaine a l'envers, puis confitions pour soustraire ou aditionner selon le chiffre romain

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,13 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
     dico_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-
-  
+    total = 0
+    valeur_precedente = 0
+    for char in reversed(roman_string):
+        value = dico_roman.get(char, 0)
+        if value < prev:
+            total -= value
+        else:
+            total += value
+        prev = value
+    return (total)
