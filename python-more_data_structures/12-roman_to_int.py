db8d49c5023551eb644cee4d4082da5e181b6140
Victoire07 <victoireboutin07@gmail.com>
2025-05-15 23:46:31 +0200

    ajout d'un dico permettant de traduire la valeur en entier d'1 chiffre romain genre I = 1 ...

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,4 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
+    dico_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+
   
