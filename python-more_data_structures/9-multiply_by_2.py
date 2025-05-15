16d1f34c488c9751de517cd7a970f7ad570dd9f9
Victoire07 <victoireboutin07@gmail.com>
2025-05-15 22:56:04 +0200

    Deuxième version non fonctionnel ne multiplie pas par 2 :(

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,6 @@
 def multiply_by_2(a_dictionary):
     nouveau_dico = {}
     for key, value in a_dictionary.items():
-        value * 2
-        nouveau_dico = nouvelle_valeur
-        return()
-
+        nouvelle_valeur = value * 2
+        nouveau_dico[key] = nouvelle_valeur
+    return()
