347b00dec6baa8f3223cdb031dc3140ba9ad676f
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 01:15:59 +0200

    Pycodestyle norme

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,13 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
-    dico_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dico_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
     total = 0
     valeur_precedente = 0
     for char in reversed(roman_string):
         value = dico_roman.get(char, 0)
-        if value <  valeur_precedente:
+        if value < valeur_precedente:
             total -= value
         else:
             total += value
