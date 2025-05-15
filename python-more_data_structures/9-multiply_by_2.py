0b11155ed04b7dbd2123fd3b8d76f5aca8f23af9
Victoire07 <victoireboutin07@gmail.com>
2025-05-15 22:53:36 +0200

    Premiere version code

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    nouveau_dico = {}
+    for key, value in a_dictionary.items():
+        value * 2
+        nouveau_dico = nouvelle_valeur
+        return()
+
