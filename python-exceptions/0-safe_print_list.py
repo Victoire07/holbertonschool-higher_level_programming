390555c9eb246ce426091e29dfad490cc96d7997
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 10:15:08 +0200

    ajout du print() pour pas tt sur 1 ligne

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     compteur = 0
-    
+    for i in range(x):
+        try:
+            print(my_list[iÒ], end="")
+            compteur += 1
+        except IndexError:
+            break
+    print()
