3bd53f2eef199eef1c108fd9966e6b3f87173db3
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 13:14:16 +0200

    modif print dans la boucle ajout de {} pour bon affichage entier

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     compteur = 0
     for i in range(x):
         try:
-            print(":d".format(my_list), end="")
+            print("{:d}".format(my_list[i]), end="")
             compteur += 1
         except:
-            print()
+    print()
+    return (compteur)
 
