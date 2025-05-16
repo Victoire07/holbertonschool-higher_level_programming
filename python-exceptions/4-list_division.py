80836fb03420848be2aa7c11e7e1b16c41431349
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 14:05:57 +0200

    début fonction avec le try créa variable et resultat de la div

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    resultats = []
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            resultats = a / b
+        except(IndexError):
