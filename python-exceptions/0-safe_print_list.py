37d926d77cc9914e46b77448fca0d2f6e24aa4a0
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 10:16:39 +0200

    Modif faute de frappe print boucle try + ajout return

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     compteur = 0
     for i in range(x):
         try:
-            print(my_list[iÒ], end="")
+            print(my_list[i], end="")
             compteur += 1
         except IndexError:
             break
     print()
+    return (compteur)
