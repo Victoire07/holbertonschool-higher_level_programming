5e241eaa44c0e9a9acdb3dbded6721d043b9030f
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 13:11:19 +0200

    1er essai fonction qui permet filtrage de types, parcours contrôlé, et gestion d’exceptions avec try/except

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    compteur = 0
+    for i in range(x):
+        try:
+            print(":d".format(my_list), end="")
+            compteur += 1
+        except:
+            print()
+
