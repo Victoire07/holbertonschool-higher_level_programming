053ce3cf50fd1834db24b0b9522ba8a149cd723f
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 14:27:31 +0200

    Pycodestyle respect

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,13 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
             a = my_list_1[i]
             b = my_list_2[i]
             resultats.append(a / b)
-        except(IndexError):
+        except (IndexError):
             print("out of range")
             resultats.append(0)
-        except(ZeroDivisionError):
+        except (ZeroDivisionError):
             print("division by 0")
             resultats.append(0)
-        except(TypeError):
+        except (TypeError):
             print("wrong type")
             resultats.append(0)
         finally:
