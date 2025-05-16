d9e89c9aa9dbb5ad47f14834278538f65b967de0
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 14:26:34 +0200

    ajout du fianlly pas trop utile ici mais imposé consigne et return

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,5 +9,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except(IndexError):
             print("out of range")
             resultats.append(0)
-
-
+        except(ZeroDivisionError):
+            print("division by 0")
+            resultats.append(0)
+        except(TypeError):
+            print("wrong type")
+            resultats.append(0)
+        finally:
+            pass
+    return (resultats)
