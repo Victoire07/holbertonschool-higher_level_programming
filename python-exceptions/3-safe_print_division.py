513d8f4b028e7bfcb16f51f36819c8e286ade25b
Victoire07 <victoireboutin07@gmail.com>
2025-05-16 13:45:44 +0200

    3eme version pour effectuer une division entière avec affichage de debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,6 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         resultat_division = None
     finally:
-        
-
-
+        print("Inside result: {}".format(resultat_division))
+    return (resultat_division)
