4b55c3c92b014896ea72f1ceb791eacef78fff09
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 01:34:01 +0200

    Création fichier 0 + ajout du shebang + ajout prototype de fonction  ajout de la docstring

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+"""
+Ce module porte sur une fonction qui doit contenir 2 entiers
+
+La fonction add_integer prend 2 nombres soit int soit float.
+Si besoin elle convertit les float en int et retourne la somme.
+Si arguments pas valides, exception avec TypeError.
+"""
