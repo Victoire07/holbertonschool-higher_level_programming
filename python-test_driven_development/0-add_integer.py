7c4c3137dd96f25dd5139c50f592a564a3b2e475
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 01:36:58 +0200

    ajout docstring de la fonction

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
 """
 Ce module porte sur une fonction qui doit contenir 2 entiers
 
@@ -7,3 +6,4 @@ La fonction add_integer prend 2 nombres soit int soit float.
 Si besoin elle convertit les float en int et retourne la somme.
 Si arguments pas valides, exception avec TypeError.
 """
+def add_integer(a, b=98):
