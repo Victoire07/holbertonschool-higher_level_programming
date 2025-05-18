27dc65f98f73e6af71e58a60d0aaba50661f7fa0
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 18:53:57 +0200

    docstring du module et de la fonction

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Ce module a pour but d'afficher du texte avec une mise en page spéciale
+"""
+
+
+def text_indentation(text):
+    """
+    Affiche le texte avec 2 retours à la ligne après .,?:
+    Supprime les espaces au début de chaque nouvelle ligne
+    """
