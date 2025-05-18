413a725bae32e06bb382fc91be36a4a0c8077c3e
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 02:00:12 +0200

    Ajout de la fonction add_integer avec vérification des types, conversion float en int, gestion des exceptions TypeError, et couverture complète des tests doctest (8/8 passés).

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,4 +6,18 @@ La fonction add_integer prend 2 nombres soit int soit float.
 Si besoin elle convertit les float en int et retourne la somme.
 Si arguments pas valides, exception avec TypeError.
 """
+
+
 def add_integer(a, b=98):
+    """
+    Additionne deux entiers ou flottants convertis en entiers.
+    Retourne leur somme sous forme d'entier.
+    Lève TypeError si un argument n’est pas un nombre.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
