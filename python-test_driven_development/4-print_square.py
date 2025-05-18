9968328d16300b5b352bced903e06fd08e1cab12
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 18:30:13 +0200

    Docstring

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Ce module a pour but d'afficher un carré avec des caractères dièzes(#)
+"""
+
+
+def print_square(size):
+    """
+    Va afficher 'My name is <first_name> <last_name>'.
+    Les deux paramètres doivent être des chaînes de caractères.
+    """
