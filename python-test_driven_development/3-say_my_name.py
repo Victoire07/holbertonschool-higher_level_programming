54ddff71e2cb58c6ebe0aa5d8225011f17241810
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 17:26:40 +0200

    Docstring

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Ce module a pour but d'afficher le prénom et le nom
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Va afficher 'My name is <first_name> <last_name>'.
+    Les deux paramètres doivent être des chaînes de caractères.
+    """
