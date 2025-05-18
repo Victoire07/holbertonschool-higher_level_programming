a64487ad8905d12d72230bbab7ed2d01d960a35e
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 02:12:40 +0200

    ajout docstrings

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""
+Ce module a pour but de diviser tous les éléments donnés par un diviseur
+"""
 def matrix_divided(matrix, div):
+    """
+    Divise tous les éléments d'une matrice par `div`, arrondi à 2 décimales.
+    """
