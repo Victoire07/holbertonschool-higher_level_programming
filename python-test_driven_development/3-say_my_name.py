4171213ce5145fc2d8fafd12385f8b40170745c4
Victoire07 <victoireboutin07@gmail.com>
2025-05-18 17:37:46 +0200

    Modification pour respect de la norme pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,13 +3,14 @@
 Ce module a pour but d'afficher le prénom et le nom
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Va afficher 'My name is <first_name> <last_name>'.
     Les deux paramètres doivent être des chaînes de caractères.
     """
-    if not isinstance (first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance (last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
