dac02a4f6a0753e14311d4b0814843e81114edbc
Victoire07 <victoireboutin07@gmail.com>
2025-05-20 23:40:56 +0200

    pycode style

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -41,11 +41,10 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def my_print(self):
         if self.size <= 0:
             print()
         else:
             for i in range(self.size):
-                print ("#" * self.size)
-
+                print("#" * self.size)
