8f7b3094211f16472698c21002dd0f55a003882f
Victoire07 <victoireboutin07@gmail.com>
2025-05-20 15:20:46 +0200

    pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,10 +6,10 @@ Création d'une classe vide appelée square
 Définir la classe sans lui donner des propriétés ou comportements
 """
 
+
 class Square:
     """
     Classe vide qui définit un carré.
-    
     Cette classe sera mise à jour plus tard
     """
     pass
