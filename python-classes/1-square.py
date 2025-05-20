4cbc80ed7bac8dcfcf6c770379a2df7d7cb2eef2
Victoire07 <victoireboutin07@gmail.com>
2025-05-20 12:54:13 +0200

    passation de l'attribut en mode privéee donc ajout des 2 _

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+"""
+Module 1-square.py
+
+Ce module définit une classe Square pour définir un carré par sa taille.
+"""
+class Square:
+    def __init__(self, size):
+        self.__sizesize = size
+
