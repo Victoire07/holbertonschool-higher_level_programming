27781a4d4e18dd47f47d8a7286d222f03c1226eb
Victoire07 <victoireboutin07@gmail.com>
2025-05-20 17:19:59 +0200

    jsp ??

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -24,5 +24,5 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        def getArea(self):
-            return (self.__size ** 2)
+    def getArea(self):
+        return (self.__size ** 2)
