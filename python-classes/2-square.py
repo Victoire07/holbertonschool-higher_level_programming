3500b5efb98ef783b630848b5bd91eda3b3fadb5
Victoire07 <victoireboutin07@gmail.com>
2025-05-20 16:08:18 +0200

    Ajout de la docstrings

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
 """
+Module 2-square.py
 
+Ce module définit une classe Square avec un attribut privé __size,
+et valide que la taille est un entier positif ou nul!!
 """
 class Square:
+    """
+    Classe représentant un carré avec taille privée validée
+    """
     def __init__(self, size = 0):
+        """
+        Initialise un carré avec une taille.
+        Args: size (int): La taille du carré (doit être un entier >=0)
+        Raises:TypeError: Si size n'est pas un entier ou ValueError:
+        Si size est < 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
