56670e12a5d005e08fc75cf2d6b7a40884c6a36f
Victoire07 <victoireboutin07@gmail.com>
2025-05-22 16:56:20 +0200

    Début de l'exo 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+Module 1-rectangle.py
+
+Ce module définit 1classe rectangle qui a :
+° deux attributs privés : __width et __height
+° des getters et setters pour accéder à ces attributs avec des vérifications
+° un constructeur __init__ qui accepte width et height en paramètres
+"""
+class Rectangle:
+    """
+    Classe Rectangle qui définit un rectangle
+    avec des attributs privés et des accesseurs contrôlés
+    """
+
+    def __init__ (self, width=0, height=0):
+       """
+       Initialise un nouveau rectangle.
+       Args:
+       ° width (int): largeur du rectangle (par défaut 0)
+       ° height (int): hauteur du rectangle (par défaut 0)
+       Raises:
+       ° TypeError: si width ou height ne sont pas des entiers
+       ° ValueError: si width ou height sont négatifs
+        """
+       self.width = width
+       self.height = height
+
+    @property
+    def width(self):
+        """
+        Getter de la largeur du rectangle.
+        Retourne la valeur de l'attribut privé __width.
+        """
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter de la largeur avec vérifications :
+        - doit être un entier
+        - doit être >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError(" width must be >= 0")
+           
