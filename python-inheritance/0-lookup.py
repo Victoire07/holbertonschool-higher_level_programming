8bb64b529b1151a6d3544ce4d9bdbff561a11467
Victoire07 <victoireboutin07@gmail.com>
2025-05-26 14:18:46 +0200

    donnation des permissions, + création de la fonction qui prend 1 paramètre : obj et retourne une liste de tous les attribs et  méthods de obj

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    Fonction qui prend un paramètre obj
+    Cette fonction doit retourner une liste de tous
+    les attributs et méthodes de obj
+    """
+    return dir(obj)
