d68f7c7b7985a27a67f80c798bb9799036f4d58e
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 13:07:56 +0200

    ajout du type car si obj exactement le même que a_class on veut que ca donne faux

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Ce module fournit une fonction pour vérifier si un objet
+est une instance d'une sous-classe d'une classe donnée (mais pas de la classe elle-même).
+"""
+def inherits_from(obj, a_class):
+    """
+    Vérifie si obj est une instance d'une sous-classe de a_class (directement ou indirectement).
+    Args: obj: L'objet à tester / a_class: La classe à comparer
+    Returns: True si obj est une instance d'une classe qui hérite de a_class
+    (mais pas a_class lui-même),False sinon.
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return (True)
+    else:
+        return (False)
