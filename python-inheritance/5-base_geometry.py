694ac0c1c0a609864903d0f6024f0a10660c6c91
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 13:29:53 +0200

    création d'une classe vide appelé BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Fonction qui consiste à écrire une classe vide appelée BaseGeometry
+"""
+class BaseGeometry:
+    """
+    Classe vide qui sera mise à jour plus tard
+    """
+    pass
