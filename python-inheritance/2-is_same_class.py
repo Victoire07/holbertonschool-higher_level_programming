4abd1fecaad8aa7e08ee97c46f52fed54b2d5e72
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 12:23:41 +0200

    type de retour boleen et pas de string

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,11 +3,13 @@
 Fonction qui renvoie True si l'objet est exactement une instance de la classe
 spécifiée ; sinon False
 """
+
+
 def is_same_class(obj, a_class):
     """
     Vérifie si l'objet est exactement une instance de la classe spécifiée
     """
     if type(obj) == a_class:
-        return ("True")
+        return (True)
     else:
-        return ("False")
+        return (False)
