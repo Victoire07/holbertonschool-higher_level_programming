86d5c435f9c353cf990b7be6cc429508ad75b47e
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 14:05:01 +0200

    Base Q°5 + ajouter une méthode publique area qui, lorsqu'elle est appelée, doit lever une exception. L'exception doit contenir le message 'area() is not implemented'

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Fonction qui consiste à écrire une classe vide appelée BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Classe vide qui sera mise à jour plus tard
+    """
+    def area(self):
+        """
+        Méthode qui lève une exception pour indiquer
+        que le calcul de l'aire n'est pas encore implémenté.
+        """
+        raise Exception("area() is not implemented")
