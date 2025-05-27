727b32ba4069cde5b538b1ed05b8b43d0c510150
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 11:05:09 +0200

    ajout de je ne sais pas

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_sorted(self):
+    """
+    Créer une classe personnalisée appelée MyList qui hérite de la classe Python list,
+    c’est-à-dire une classe qui se comporte comme une liste normale
+    mais avec une fonction en plus : print_sorted().
+    """
+    class MyList(list):
+        """
+        """
+        print(sorted(self))
