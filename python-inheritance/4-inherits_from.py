9dcaf4c2f128e96158e7bf096f1757a69020904f
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 13:11:32 +0200

    Pycodestyle norme

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
 Ce module fournit une fonction pour vérifier si un objet
-est une instance d'une sous-classe d'une classe donnée (mais pas de la classe elle-même).
+est une instance d'une sous-classe d'une classe donnée
+(mais pas de la classe elle-même)
 """
+
+
 def inherits_from(obj, a_class):
     """
-    Vérifie si obj est une instance d'une sous-classe de a_class (directement ou indirectement).
+    Vérifie si obj est une instance d'une sous-classe de a_class
+    (directement ou indirectement).
     Args: obj: L'objet à tester / a_class: La classe à comparer
     Returns: True si obj est une instance d'une classe qui hérite de a_class
     (mais pas a_class lui-même),False sinon.
