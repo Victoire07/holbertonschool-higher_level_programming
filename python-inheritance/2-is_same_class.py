d05e2cda467ba936055b76af5286a98d71e0eee0
Victoire07 <victoireboutin07@gmail.com>
2025-05-27 12:17:57 +0200

    fonction pour vérifier si objet est instance de classe spécifiée mais quand je teste j'ai ce qui est attendu et des lignes supplémentaires donc à retravailler

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Fonction qui renvoie True si l'objet est exactement une instance de la classe
+spécifiée ; sinon False
+"""
+def is_same_class(obj, a_class):
+    """
+    Vérifie si l'objet est exactement une instance de la classe spécifiée
+    """
+    if type(obj) == a_class:
+        return ("True")
+    else:
+        return ("False")
