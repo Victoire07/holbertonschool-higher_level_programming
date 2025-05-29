3c5dcd9a8192a4ce24f0fc9b663b2e372196f551
Victoire07 <victoireboutin07@gmail.com>
2025-05-29 13:41:49 +0200

    permission + aire + str pour description

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Le module porte sur la classe Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Classe qui hérite de BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Initialise un rectangle avec une largeur et une hauteur
+        validées comme entiers strictement positifs.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calcule et retourne l'aire du rectangle
+        """
+        return self.__height * self.width
+
+    def __str__(self):
+        """
+        Permet de retourner une discription lisible et explicative
+        """
+        return (f"[Rectangle] {self.__width}/{self.__height}")
