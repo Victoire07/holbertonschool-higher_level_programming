5bebe0a85b6ec446958ebdadbb9a8ca8d9f35e58
Victoire07 <victoireboutin07@gmail.com>
2025-05-29 17:49:00 +0200

    début de l'exo création de la class abnstraite

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module exercice task_01_duck_typing
+"""
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    """
+    Fais moi la doctring poir ici stpp
+    """
+    @abstractmethod
+
+
