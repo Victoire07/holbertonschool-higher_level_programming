5db11ce93d442c9ceedca86fe400f73e1cd9b899
Victoire07 <victoireboutin07@gmail.com>
2025-05-30 00:57:05 +0200

    Interface commune a toutes les formes avec le prérimètre et l'aire

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -5,8 +5,22 @@ Module exercice task_01_duck_typing
 from abc import ABC, abstractmethod
 class Shape(ABC):
     """
-    Fais moi la doctring poir ici stpp
+    Classe abstraite représentant une forme géométrique.
+
+    Cette classe définit l'interface que toutes les formes doivent implémenter,
+    notamment les méthodes `area` pour calculer la surface et `perimeter`
+    pour calculer le périmètre. Elle sert de modèle commun pour toutes
+    les formes géométriques (cercle, rectangle, etc.).
     """
     @abstractmethod
+    def area(self):
+        """
+        Permet de calculer la surface
+        """
+    def perimeter(self):
+        """
+        Permet de calculer le périmètre
+        """
+        pass
 
 
