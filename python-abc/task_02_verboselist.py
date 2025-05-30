2f197a8352b63cf5894fdb610e9fa5ca08407140
Victoire07 <victoireboutin07@gmail.com>
2025-05-30 10:34:00 +0200

    Création classe perso Verboselist hérité de list (class python)

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Fais moi une docstring courte concise 
+"""
+class VerboseList(list):
+    """
+    Classe personnalisée VerboseList qui hérite de la classe Python list,
+    mais affiche un message à chaque ajout ou suppression d’un élément
+    """
+    
