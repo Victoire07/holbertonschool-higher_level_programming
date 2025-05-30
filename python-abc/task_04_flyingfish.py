c18e94e65e1ce69f6c0d6a269922e65e92082155
Victoire07 <victoireboutin07@gmail.com>
2025-05-30 16:22:42 +0200

    créa des class fish et bird + les defs

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Fish:
+    def swim(self):
+        return ("The fish is swimming")
+
+    def habitat(self):
+        return ("The fish lives in water")
+
+class Bird:
+    def fly(self):
+        return ("The bird is flying")
+
+    def habitat(self):
+        return ("The bird lives in the sky")
+    
