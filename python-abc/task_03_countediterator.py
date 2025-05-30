f17e674c974f1e03360c46954ba0e02c3f007c70
Victoire07 <victoireboutin07@gmail.com>
2025-05-30 13:00:38 +0200

    ajout de la class CountedIterator + def init

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module exercice task_03_countediterator
+"""
+class CountedIterator:
+    """
+    Fais moi la docstring stpp
+    """
+    def __init__(self, iterator, count):
+        """
+        Fais moi la docstring stp
+        """
+        self.iterator = iterator
+        self.count = count
+        
