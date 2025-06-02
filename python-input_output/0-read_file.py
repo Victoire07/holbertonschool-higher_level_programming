bc52c24155924fdba55d896a5cfabba465afe854
Victoire07 <victoireboutin07@gmail.com>
2025-06-02 16:42:01 +0200

    pycodestyle modfi

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file.py
+"""
+
 
 def read_file(filename=""):
     """
     Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard
     """
+
     with open(filename, "r") as f:
         print(f.read())
