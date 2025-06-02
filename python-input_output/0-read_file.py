8f8cd53263ba8144934030ea7b196fab9a88389b
Victoire07 <victoireboutin07@gmail.com>
2025-06-02 16:49:34 +0200

    ajout utf8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard
     """
 
-    with open(filename, "r") as f:
-        print(f.read())
+    with open(filename, "r", encoding = "utf-8") as f:
+        print(f.read(), end="")
