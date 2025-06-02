15fd7755d33858ca24be1b86189948030b6f5bdf
Victoire07 <victoireboutin07@gmail.com>
2025-06-02 16:35:36 +0200

    IDK

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@ def read_file(filename=""):
     """
     Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard
     """
-    with open("filename", "r") as f:
+    with open(filename, "r") as f:
         for ligne in f:
             print(ligne)
