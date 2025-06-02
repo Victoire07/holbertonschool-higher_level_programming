a8b81ea661eb5673dd54fef08cc0fd56a64a19e0
Victoire07 <victoireboutin07@gmail.com>
2025-06-02 16:40:15 +0200

    modification pour le print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,4 @@ def read_file(filename=""):
     Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard
     """
     with open(filename, "r") as f:
-        for ligne in f:
-            print(ligne)
+        print(f.read())
