867fa8ca2db592d1676933f87a3ff180f62d11c9
Victoire07 <victoireboutin07@gmail.com>
2025-06-02 16:32:12 +0200

    modif fichier main + permissions + lecture fichier q0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard
+    """
+    with open("filename", "r") as f:
+        for ligne in f:
+            print(ligne)
