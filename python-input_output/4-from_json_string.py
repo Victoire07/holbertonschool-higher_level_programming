0ec632c9083b36c0b35d41eb60d6e24867c3e92d
Victoire07 <victoireboutin07@gmail.com>
2025-06-03 16:45:39 +0200

    fonction qui converti JSON en Python

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string.py
+"""
+import json
+
+def from_json_string(my_str):
+    """
+    Fonction qui reçoit une chaîne de caractères au format JSON
+    et renvoie l’objet Python correspondant (liste, dictionnaire...)
+    """
+    return json.loads(my_str)
