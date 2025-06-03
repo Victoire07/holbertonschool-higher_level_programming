e56fc4df6251079f4c8778f2741d42e05933714b
Victoire07 <victoireboutin07@gmail.com>
2025-06-03 11:58:50 +0200

    permission des 2 fichiers avce chmod + ecrire string et return nb de cara ecrits

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 1-write_file.py
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Écrit une chaîne de caractères dans un fichier texte (encodé en UTF-8) et
+    retourne le nombre de caractères qui ont été écrits
+    """
+
+    with open(filename, "w", encoding="utf_8") as f:
+        print(f.write(), end="")
