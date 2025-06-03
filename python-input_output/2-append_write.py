c2f4b5b2aee6de1c556da6b53daad2c2e96e90ac
Victoire07 <victoireboutin07@gmail.com>
2025-06-03 12:25:04 +0200

    pycodestyle modif + permissions des deux fichiers

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,4 +9,5 @@ def append_write(filename="", text=""):
     Fonction qui ajoute du texte à la fin d’un fichier texte (encodé en UTF-8)
     et retourne le nombre de caractères ajoutés
     """
-
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
