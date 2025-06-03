6a30cb2177ed02075bdb8e6405b83c526149a599
Victoire07 <victoireboutin07@gmail.com>
2025-06-03 12:24:06 +0200

    créa fct ajte texte a la fin fichier texte

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 2-append_write.py
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Fonction qui ajoute du texte à la fin d’un fichier texte (encodé en UTF-8)
+    et retourne le nombre de caractères ajoutés
+    """
+
