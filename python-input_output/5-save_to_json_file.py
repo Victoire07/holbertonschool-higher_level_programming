88353f9d487a4c186b22eba9a62bf3d174325e21
Victoire07 <victoireboutin07@gmail.com>
2025-06-03 17:15:37 +0200

    Fonction qui prend un obj Pyth et enregsitre ds 1 fich sous forme JSON

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,3 +2,13 @@
 """
 Module 5-save_to_json_file.py
 """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Fonction qui prend un objet Python (ex:une liste ou un dictionnaire)
+    et l’enregistre dans un fichier texte sous forme JSON
+    """
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
