afe7ccd7cb41dfdf7c11361c2b7ea6534818ca44
Victoire07 <victoireboutin07@gmail.com>
2025-06-04 15:11:38 +0200

    permission + créa class et initialisation

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 9-student.py
+"""
+
+class Student():
+    """
+    Classe représentant 1 élève avec nom, prénom et âge
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialise un étudiant avec son prénom, nom et âge
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
