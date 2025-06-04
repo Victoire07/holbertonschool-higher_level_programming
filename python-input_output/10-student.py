e348e0dcd199bb23b55645518d34c07cc027949b
Victoire07 <victoireboutin07@gmail.com>
2025-06-04 15:34:23 +0200

    ajout des permissions plus conditions if else

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module 10-student.py
+"""
+
+
+class Student():
+    """
+    Classe représentant 1 élève avec nom, prénom et âge
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialise un étudiant avec son prénom, nom et âge
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retourne un dictionnaire représentant les attributs de l'étudiant
+        """
+        return self.__dict__
