40af3e30df99227cf89cc4c5ec88d426d902f835
Victoire07 <victoireboutin07@gmail.com>
2025-06-04 14:26:53 +0200

    ajout des permissions pour tous les fichiers questions 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,3 +2,12 @@
 """
 Module 8-class_to_json.py
 """
+
+
+def class_to_json(obj):
+    """
+    Renvoie la description du dictionnaire pour la sérialisation JSON d1 objet
+    """
+    return obj.__dict__
+
+
