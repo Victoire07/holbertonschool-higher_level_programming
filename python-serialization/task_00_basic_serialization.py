d8eb610183ceb056301287502cf0f734b62c7ec3
Victoire07 <victoireboutin07@gmail.com>
2025-06-05 16:10:13 +0200

    deserialisation

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -11,3 +11,12 @@ def serialize_and_save_to_file(data, filename):
     """
     with open(filename, "w") as f:
         json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """
+    Charge les données sérialisées à partir d'un fichier JSON
+    et les désérialise en un dictionnaire Python
+    """
+    with open(filename, "r") as f:
+        data=json.load(f)
+        
