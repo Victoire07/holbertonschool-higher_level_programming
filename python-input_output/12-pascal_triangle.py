78c547bf93d3f939b3afe572890af0ab856b4708
Victoire07 <victoireboutin07@gmail.com>
2025-06-05 12:06:40 +0200

    prototype + docstring fonction

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module 12-pascal_triangle.py
+"""
+def pascal_triangle(n):
+    """
+    Fonction qui retourne le triangle de Pascal,
+    sous forme de liste de listes de nombres entiers
+    """
