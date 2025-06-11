6850f6237dff82358e28f8fc390583f6e31736aa
Victoire07 <victoireboutin07@gmail.com>
2025-06-12 00:47:59 +0200

    Ajout de la fonction fetch_and_save_posts : récupération des posts et sauvegarde au format CSV avec les champs id, title et body

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -26,5 +26,19 @@ def fetch_and_save_posts():
     variable_reponse = requests.get("https://jsonplaceholder.typicode.com/posts")
     if variable_reponse.status_code == 200:
         print("Status Code: 200")
+        variable_list_posts = variable_reponse.json()
+
+        variable_donnees_filtrees = []
+        for variable_un_post in variable_list_posts:
+            variable_donnees_filtrees.append({
+                "id": variable_un_post["id"],
+                "title": variable_un_post["title"],
+                "body": variable_un_post["body"]
+                })
+        with open("posts.csv", "w", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(variable_donnees_filtrees)
+
     else:
         print("Erreur", variable_reponse.status_code)
