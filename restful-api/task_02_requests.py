9427c286bd1a6d7d1dc0f4eb6b8861335765edae
Victoire07 <victoireboutin07@gmail.com>
2025-06-11 16:54:46 +0200

    ajout bibliotheque requests + def fonction

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Module task_02_requests
+"""
+
+import requests
+
+def fetch_and_print_posts():
+    """
+    Fonction qui va afficher chaque titre de post
+    """
+    variable_reponse = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
+
+
