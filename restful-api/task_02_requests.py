250d8c1f0f63eab241dec5779a4a95bff8e23daf
Victoire07 <victoireboutin07@gmail.com>
2025-06-11 17:14:29 +0200

    Ajout de la condition si pour print 200 si reussite et erreur si echec

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -10,6 +10,7 @@ def fetch_and_print_posts():
     Fonction qui va afficher chaque titre de post
     """
     variable_reponse = requests.get("https://jsonplaceholder.typicode.com/posts")
-    
-
-
+    if variable_reponse.status_code == 200:
+        print("Status Code: 200")
+    else:
+        print("Erreur", variable_reponse.status_code)
