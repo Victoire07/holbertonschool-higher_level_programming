8825d959a985d788a1a5b6a2683628164c8daf85
Victoire07 <victoireboutin07@gmail.com>
2025-06-12 15:52:43 +0200

    supression d'un espace inutile pour status et ajout d'une ligne mnquante

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -20,14 +20,21 @@ class API_Server(http.server.BaseHTTPRequestHandler):
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode("utf-8"))
 
-        elif self.path == "/status ":
+        elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write("OK".encode("utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Endpoint not found".encode("utf-8"))
+    
 
 
