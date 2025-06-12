05377aee412090650be931dfa1197aab0ae3e771
Victoire07 <victoireboutin07@gmail.com>
2025-06-12 16:31:26 +0200

    ajout de info que j'avais oublié ...

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,7 +12,7 @@ class API_Server(http.server.BaseHTTPRequestHandler):
     Classe représentant un serveur HTTP simple. Elle hérite de
     BaseHTTPRequestHandler et redéfinit la méthode do_GET pour gérer les
     requêtes GET sur différents endpoints :
-    
+
     - / : renvoie un message texte "Hello, this is a simple API!"
     - /data : renvoie un dictionnaire encodé en JSON avec des données fictives
     - /status : renvoie "OK" pour indiquer que le serveur est opérationnel
@@ -21,18 +21,14 @@ class API_Server(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """
         Gère les requêtes HTTP GET.
-        Selon le chemin de la requête (self.path), le serveur renvoie une réponse appropriée :
-        ° 200 OK avec un message texte pour `/`
-        ° 200 OK avec des données JSON pour `/data`
-        ° 200 OK avec "OK" pour `/status`
-        ° 404 Not Found pour tout autre chemin
+        Selon le chemin de la requête (self.path)le serveur renvoie une réponse
+        ° 200 : OK ou 404 : Not Found
         """
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-
 
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
@@ -45,13 +41,21 @@ class API_Server(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"OK".encode("utf-8"))
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            data = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
 
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not found".encode("utf-8"))
+
 
 if __name__ == "__main__":
     PORT = 8000
