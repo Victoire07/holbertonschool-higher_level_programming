58b0550088db7ab321692615724aea01bef9c195
Victoire07 <victoireboutin07@gmail.com>
2025-06-12 16:11:29 +0200

    ajout des docstrings

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,9 +8,23 @@ import http.server
 
 class API_Server(http.server.BaseHTTPRequestHandler):
     """
+    Classe représentant un serveur HTTP simple. Elle hérite de
+    BaseHTTPRequestHandler et redéfinit la méthode do_GET pour gérer les
+    requêtes GET sur différents endpoints :
+    
+    - / : renvoie un message texte "Hello, this is a simple API!"
+    - /data : renvoie un dictionnaire encodé en JSON avec des données fictives
+    - /status : renvoie "OK" pour indiquer que le serveur est opérationnel
+    - Tout autre chemin : renvoie une erreur 404 "Endpoint not found"
     """
     def do_GET(self):
         """
+        Gère les requêtes HTTP GET.
+        Selon le chemin de la requête (self.path), le serveur renvoie une réponse appropriée :
+        ° 200 OK avec un message texte pour `/`
+        ° 200 OK avec des données JSON pour `/data`
+        ° 200 OK avec "OK" pour `/status`
+        ° 404 Not Found pour tout autre chemin
         """
         if self.path == "/":
             self.send_response(200)
