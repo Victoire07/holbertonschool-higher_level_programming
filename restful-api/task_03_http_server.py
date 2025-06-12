42b7112210568bc605bbc8f64d5c5804a050ca13
Victoire07 <victoireboutin07@gmail.com>
2025-06-12 15:48:16 +0200

    Prmeiere version pour task 3 avec toutes le bloc conditionnel mais non fonctionnel

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,7 +3,7 @@
 task_O3-http_server.py
 """
 import json
-import http
+import http.server
 
 class API_Server(http.server.BaseHTTPRequestHandler):
     """
@@ -11,4 +11,23 @@ class API_Server(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """
         """
-        
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode("utf-8"))
+
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/status ":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("OK".encode("utf-8"))
+
+
