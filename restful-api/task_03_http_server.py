1c68332aa53d74e4f8ca6a3897ed88cbda04d07c
Victoire07 <victoireboutin07@gmail.com>
2025-06-12 15:18:00 +0200

    créa class et def de do GET

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+task_O3-http_server.py
+"""
+import json
+import http
+
+class API_Server(http.server.BaseHTTPRequestHandler):
+    """
+    """
+    def do_GET(self):
+        """
+        """
+        
