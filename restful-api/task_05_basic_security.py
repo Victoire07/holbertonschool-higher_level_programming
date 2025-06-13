2c542e04380c53419f48de814dfebe4b1e795be3
Victoire07 <victoireboutin07@gmail.com>
2025-06-13 17:26:07 +0200

    importation pour pouvoir lire les fichiers en JSON etc

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+task_05_basic_security
+"""
+from flask import Flask, jsonify, request
+from flask_httpauth import HTTPBasicAuth
+from flask_jwt_extended import (
+    JWTManager, create_access_token,
+    jwt_required, get_jwt_identity
+)
+from werkzeug.security import generate_password_hash, check_password_hash
