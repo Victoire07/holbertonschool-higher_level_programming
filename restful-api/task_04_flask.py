5d78feb42af08891f0b6bd0a7b2b2b384ac77135
Victoire07 <victoireboutin07@gmail.com>
2025-06-13 14:11:31 +0200

    debut de fnctn

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,3 +2,13 @@
 """
 task_04_flask.py
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+if __name__ == "__main__":
+    app.run()
