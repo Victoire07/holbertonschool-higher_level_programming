d976679adeb30afce2319a37246df2b62fc7c81d
Victoire07 <victoireboutin07@gmail.com>
2025-06-13 15:08:44 +0200

    ajout de sttaus, basique / et data

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,13 +2,25 @@
 """
 task_04_flask.py
 """
-from flask import Flask
+import jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+users = {}
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
+@app.route("/data")
+def get_usernames():
+    return jsonify(list(users.keys()))
+
+@app.route("status")
+def status():
+    return("OK")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
