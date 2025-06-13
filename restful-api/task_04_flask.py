ed5c40e5925a7914c8a3baf5ca28e07c97cd6c8d
Victoire07 <victoireboutin07@gmail.com>
2025-06-13 15:16:13 +0200

    ajout de la route POST /add_user pour ajouter un utilisateur

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -15,13 +15,32 @@ def home():
 
 @app.route("/data")
 def get_usernames():
-    return jsonify(list(users.keys()))
+    return (jsonify(list(users.keys())))
 
 @app.route("status")
 def status():
-    return("OK")
+    return ("OK")
 
+@app.route("/users/<username>")
+def get_user(username):
+    if username in users:
+        return (jsonify(users[username]))
+    else:
+        return (jsonify({"error": "User not found"}), 404)
+    
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+
+    if not data or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+
+    username = data["username"]
+    users[username] = data
+
+    return jsonify({"message": "User added", "user": data}), 201
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
+
 
