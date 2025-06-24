1bbc26d5319172c6086ee4ebda6c75c599446e04
Victoire07 <victoireboutin07@gmail.com>
2025-06-24 16:17:26 +0200

    2ÈME VERSION

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,3 @@
 -- Script qui compte combien de lignes dans la table first_table ont la valeur id = 89
 
-COUNT id = 89 IN first_table;
-SELECT COUNT(id = 89) FROM first_table;
+COUNT (*) WHERE id = 89 IN first_table;
