72729ebbad3ae940d1f3505d626bdf7d5310fc48
Victoire07 <victoireboutin07@gmail.com>
2025-06-24 16:36:41 +0200

    ajout de 2 fncts dans le code 1 pour les colonnes et 1 pour les lignes et ajout des , et du ;

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,9 @@
--- Créer une nouvelle table appelée second_table dans la base hbtn_0c_0, et y insérer plusieurs lignes dès la création
-
+-- Créer une nouvelle table appelée second_table dans la base hbtn_0c_0 avec 3 colonnes !
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+
+-- Insérer dans cette table plusieurs lignes
 INSERT INTO second_table (id, name, score) VALUES
-(1, 'John', 10)
-(2, 'Alex', 3)
-(3, 'Bob', 14)
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
 (4, 'George', 8);
