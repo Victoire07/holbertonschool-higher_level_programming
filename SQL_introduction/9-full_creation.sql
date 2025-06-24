005287f3cdbf7b73798ad0ce63983da6a3528b2b
Victoire07 <victoireboutin07@gmail.com>
2025-06-24 16:34:53 +0200

    ajout d'une version non fonctionnel ://

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- Créer une nouvelle table appelée second_table dans la base hbtn_0c_0, et y insérer plusieurs lignes dès la création
 
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
-   INSERT INTO second_table
-      (column1, column2,column3, column4)
-   VALUES (value-for-column1, value-for-column2, value-for-column3, value-for-column4)
-
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10)
+(2, 'Alex', 3)
+(3, 'Bob', 14)
+(4, 'George', 8);
