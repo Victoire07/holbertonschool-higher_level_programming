4f418bc505ed8b6553af27497e1c136886e55f8a
Victoire07 <victoireboutin07@gmail.com>
2025-06-24 16:30:09 +0200

    création d'une 2ème table et insertion de +sieurs lignes

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Créer une nouvelle table appelée second_table dans la base hbtn_0c_0, et y insérer plusieurs lignes dès la création
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+   INSERT INTO second_table
+      (column1, column2,column3, column4)
+   VALUES (value-for-column1, value-for-column2, value-for-column3, value-for-column4)
+
