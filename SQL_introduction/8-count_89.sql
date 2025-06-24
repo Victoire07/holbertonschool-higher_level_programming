16460d2860c893a54c0797b3b21c836115dd5aae
Victoire07 <victoireboutin07@gmail.com>
2025-06-24 16:20:44 +0200

    modification COUNT(*) et pas COUNT (*)

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Script qui compte combien de lignes dans la table first_table ont la valeur id = 89
 
-SELECT COUNT (*) FROM first_table WHERE id = 89 IN first_table;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
