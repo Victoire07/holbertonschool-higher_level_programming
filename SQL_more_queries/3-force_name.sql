8e2e56b96d57cd9ac306b2a08c9b7dd6bd93b285
Victoire07 <victoireboutin07@gmail.com>
2025-06-25 22:12:03 +0200

    1ère version non fonctionnel de création table + ajout de 2 colonnes

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Crée la table force_name avec une contrainte NOT NULL sur la colonne name
+CREATE TABLE force_name
+(
+    ADD COLUMN INT int IF NOT EXISTS;
+    ADD COLUMN name VARCHAR(256) NOT NULL IF NOT EXISTS;
+)
