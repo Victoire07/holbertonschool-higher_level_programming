6d6662ae20228445287d95e7d7e899ba2242684d
Victoire07 <victoireboutin07@gmail.com>
2025-06-25 16:36:33 +0200

    ajout des ; à la fin

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Liste les privilèges de user 1 et user 2 sur la base de données hbtn_0c_0
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost'
-SHOW GRANTS FOR 'user_0d_2' @ 'localhost'
+-- Liste les privilèges de user 1 et user 2 sur local
+SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
