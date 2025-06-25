897fbaf210cbe38a0ca7e6683e11fa03f6d3d19a
Victoire07 <victoireboutin07@gmail.com>
2025-06-25 22:20:05 +0200

    Modification du nom de la colonne que j'ai appelé int au lieu de id...

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- Crée la table force_name avec une contrainte NOT NULL sur la colonne name
 CREATE TABLE IF NOT EXISTS force_name
 (
-    int INT,
+    id INT,
     name VARCHAR(256) NOT  NULL
 );
