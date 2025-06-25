7d96e5072b2a1f6a0d3fe992307dd937d07f0940
Victoire07 <victoireboutin07@gmail.com>
2025-06-25 17:16:35 +0200

    modificiation car donne les droits a toutes les BD et la ca doit etre spécifique

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Crée la base hbtn_0d_2 et l’utilisateur user_0d_2 avec droit de lecture uniquement
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
+
+
+
