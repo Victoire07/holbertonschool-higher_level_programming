c6ec40a73f792371d13b35983954c6e5c531c236
Victoire07 <victoireboutin07@gmail.com>
2025-06-25 16:37:19 +0200

    supression des espaces pour syntaxe MySQL

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Liste les privilèges de user 1 et user 2 sur local
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
-SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
