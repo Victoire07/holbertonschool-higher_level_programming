733f1306935be33e95e3481279219265b12e8b22
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 00:11:18 +0200

    ajout comm

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
--- 
+-- Crée la base hbtn_0d_usa et la table states avec id auto-incrémenté en clé primaire et name obligatoire
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states
