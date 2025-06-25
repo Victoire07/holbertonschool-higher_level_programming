16b9c12c826d62cf35ca995066294e93fc59ca59
Victoire07 <victoireboutin07@gmail.com>
2025-06-25 16:56:03 +0200

    ajout des ; pour dire ok instruction terminée

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Crée l'utilisateur user_0d_1 avec tous les privilèges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_password';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
