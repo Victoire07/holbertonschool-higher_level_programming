e36e6e48be989af6a6270cf64fb946fe2eb034a5
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 02:50:59 +0200

    Q8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Liste les villes de l'État California sans utiliser JOIN, triées par id croissant
+SELECT *
+FROM cities
+WHERE state_id = (SELECT id
+                 FROM states
+                 WHERE name = 'California')
+ORDER BY id ASC;
