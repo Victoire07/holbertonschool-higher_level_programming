974857b7c730270358b83d26df590ae08123f6ef
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 23:52:12 +0200

    IDK

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- Liste tous les genres associés à la série "Dexter", triés par ordre alphabétique du nom du genre
-SELECT tv_genres.name FROM tv_genres
+SELECT tv_genres.name
+FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
