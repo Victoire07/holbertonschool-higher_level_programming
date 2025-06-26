86e1b529a98ffdf62b0db505377341a671215926
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 15:22:06 +0200

    ajout de USE pour la BD a utilisé pour passation checker 5

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- Crée la table cities dans hbtn_0d_usa avec une clé étrangère vers la table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
