6f93c460534982d8964da292e11f54e70da33ea9
Victoire07 <victoireboutin07@gmail.com>
2025-06-27 00:02:09 +0200

    manquer les JOIN pour relier les tables entre eux et aussi ASC au lieu de DSC car croissant

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,7 @@
 -- Script qui affiche tous les titres des séries de genre "Comedy" dans la base hbtn_0d_tvshows
 SELECT tv_shows.title
-FROM tv_showstv_shows
-WHERE name in TABLE tv_genres = 'Comedy'
-ORDER BY tv_shows.title DESC;
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
