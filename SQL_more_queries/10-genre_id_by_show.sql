7f0d6cbd0db6e06af56d53e51dd66d3b4e0478b5
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 13:22:50 +0200

    modifif dans le code pour le tri ORDER BY

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- Liste les titres de séries avec au moins un genre associé, triés par titre et id de genre
-SELECT tv_shows.title, tv_show_genres.id
-INNER JOIN genres ON tv_show_genres = genres.id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.id;
-ORDER BY cities.id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
