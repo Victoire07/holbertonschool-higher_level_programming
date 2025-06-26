65447030bb1c8c688d209f7e4a740947039f6d09
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 02:40:18 +0200

    créa BD + tables + colonnes x3

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,8 @@
 -- Crée la table cities dans hbtn_0d_usa avec une clé étrangère vers la table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT FOREIGN KEY,
+    name VARCHAR(256) NOT NULL
+);
