631e2b74af83583d96169433e792f440f941c726
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 13:20:22 +0200

    modification des noms de variables

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Liste les titres de séries avec au moins un genre associé, triés par titre et id de genre
-SELECT tv_shows, tv_show_genres
+SELECT tv_shows.title, tv_show_genres.id
 INNER JOIN genres ON tv_show_genres = genres.id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, genres.id;
+ORDER BY tv_shows.title, tv_show_genres.id;
 ORDER BY cities.id ASC;
