23bb7aead038e114321ed506173c166ae42eeb70
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 14:11:34 +0200

    supression du USE non accepté pâr le checker

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,4 @@
 -- Liste toutes les séries avec leurs identifiants de genre s’ils existent, sinon NULL, triées par titre et genre_id
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
