517866f02a51eb7050314cdb112694e39e6e34a0
Victoire07 <victoireboutin07@gmail.com>
2025-06-27 00:15:57 +0200

    toujours pas trouver pour le checker donc modif ds la jointure des tables

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Script SQL qui affiche tous les shows, et pour chacun : le ou les genres associés ou NULL s’il n’a aucun genre !
-SELECT tv_shows.title AS title, tv_genres.name AS genre
+SELECT tv_shows.title AS title, tv_genres.name AS  name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
