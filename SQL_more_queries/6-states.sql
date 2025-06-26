154e3bb855e44a38fb74ff6a2b5ec9c64a147c6a
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 12:19:14 +0200

    modification dans le code pour ajouter NOTNULL dans l'id et l'use de la BD

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,10 @@
 -- Crée la base hbtn_0d_usa et la table states avec id auto-incrémenté en clé primaire et name obligatoire
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
+
