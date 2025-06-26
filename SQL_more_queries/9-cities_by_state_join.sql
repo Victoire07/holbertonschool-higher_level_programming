6392e482b3576d9b2075710e05751025d8681658
Victoire07 <victoireboutin07@gmail.com>
2025-06-26 13:08:41 +0200

    tri par id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Liste des villes avec leur État, triées par id de la table cities
+USE hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-INNER JOIN cities.state_id = states.id
+FROM cities
+JOIN cities.state_id ON states.id = cities.state_id
 ORDER BY cities.id ASC;
-
