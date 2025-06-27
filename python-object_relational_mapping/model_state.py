bf9e7c6efc3adecdd8e1807ee7eb34bd709b66e3
Victoire07 <victoireboutin07@gmail.com>
2025-06-27 23:10:23 +0200

    Modification pour le respect de la norme Pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,6 +6,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+
 class State(Base):
     """
     Classe représentant l'état dans la base de données
@@ -13,4 +15,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
