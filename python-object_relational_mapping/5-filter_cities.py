d6bec7cd6d23cf2b53ba9bc5565e7cafd56468eb
Victoire07 <victoireboutin07@gmail.com>
2025-06-27 17:11:02 +0200

    Connexion sécurisée à la base

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Afficher les villes d'un état donné (passé en argument) depuis la base de données
+hbtn_0e_4_usa, avec protection contre les injections SQL.
+Les résultats doivent être affichés sur une seule ligne, séparés par des virgules
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Print toutes les villes d'un état triées par id
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    
