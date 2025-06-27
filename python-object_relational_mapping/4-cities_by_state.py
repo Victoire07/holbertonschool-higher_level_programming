1bdcb9209aff965ab273a76d98e49dc14a903510
Victoire07 <victoireboutin07@gmail.com>
2025-06-27 16:48:20 +0200

    Pycodestyle Modification

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Afficher toutes les villes (cities) avec le nom de leur état (states) associé, depuis
-la BD hbtn_0e_4_usa
+Afficher toutes les villes (cities) avec le nom de leur état (states) associé
+depuis la BD hbtn_0e_4_usa
 """
 import sys
 import MySQLdb
