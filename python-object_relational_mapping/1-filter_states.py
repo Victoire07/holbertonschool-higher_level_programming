1f1c36edbcffd20234ea8cd77f8c1b6d7af25e59
Victoire07 <victoireboutin07@gmail.com>
2025-06-27 14:33:59 +0200

    Récupération et affichage des résultats, fermeture du curseur et de la connexion

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,4 +19,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
-
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
