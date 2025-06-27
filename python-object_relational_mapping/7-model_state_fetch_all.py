6950319dc9a9374c629884728e45f80a6465f7b0
Victoire07 <victoireboutin07@gmail.com>
2025-06-28 00:00:31 +0200

    Ajout de la session SQLAlchemy et affichage des objets State triés par id

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,3 +18,10 @@ if __name__ == "__main__":
     ),
     pool_pre_ping=True
 )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
