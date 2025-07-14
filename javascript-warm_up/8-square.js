7cc204313a4a72f6feb7ef8963f91f94786d99fc
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 17:31:59 +0200

    ajout condition pour pas ue ca print si chiffre négatif

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const argument = process.argv[2];
 const number = parseInt(argument);
 if (isNaN(number)) {
   console.log('Missing size');
-} else {
+} else if (number > 0) {
 const line = 'X'.repeat(number);
 for (let index = 0; index < number; index++) {
     console.log(line);
