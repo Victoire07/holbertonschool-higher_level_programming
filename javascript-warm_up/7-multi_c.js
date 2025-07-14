799e644485eb11705ece7845b6dd8b1688b63498
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 17:03:06 +0200

    ajout des ; mon code s'execute mais les checkers passent pas donc je tente des modifs

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@
 const argument = process.argv[2];
 const number = parseInt(argument);
 if (isNaN(number)) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
-  for(let index = 0; index < number; index++) {
-    console.log('C is fun')
+  for (let index = 0; index < number; index++) {
+    console.log('C is fun');
 }
 }
