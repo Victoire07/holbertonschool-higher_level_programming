43a2acd232f37b382a74619a6dbde339f4f2fdc7
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 18:30:41 +0200

    Modfi nom variable

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const premier_argument = process.argv[2];
 const deuxieme_argument = process.argv[3];
 const premier_nombre = parseInt(premier_argument);
 const deuxieme_nombre = parseInt(deuxieme_argument);
-function add(a, b) {
+function add(premier_nombre, deuxieme_nombre) {
     return premier_nombre + deuxieme_nombre;
 }
 console.log(add(premier_nombre +  deuxieme_nombre))
