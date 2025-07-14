26fed1f70307c04212f96456148fbf84700a9fba
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 18:17:58 +0200

    modfi pour return la somme des deux nombres et afficher les 2 nombres en additionnés

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// #!/usr/bin/env node : pour execution
+const premier_argument = process.argv[2];
+const deuxieme_argument = process.argv[3];
+const premier_nombre = parseInt(premier_argument);
+const deuxieme_nombre = parseInt(deuxieme_argument);
+function add(a, b) {
+    return premier_nombre + deuxieme_nombre;
+}
+console.log(add(premier_nombre +  deuxieme_nombre))
