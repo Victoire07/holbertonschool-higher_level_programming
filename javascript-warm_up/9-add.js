f7df424ef5ef0f18bc231d5d3b40824c87fdc07b
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 18:39:53 +0200

    modfif fonction + retour avec les bons noms de variables

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 // #!/usr/bin/env node : pour execution
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-function add(premier_nombre, deuxieme_nombre) {
-    return premier_nombre + deuxieme_nombre;
+function add(a, b) {
+    return a + b;
 }
 console.log(add(a, b))
