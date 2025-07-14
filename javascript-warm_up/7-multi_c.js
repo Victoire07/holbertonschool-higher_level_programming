d77d7354057a114579c409789c1bdfbd86e42783
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 16:55:26 +0200

    ajouter un script qui affiche 'C is fun' x fois selon l'argument

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// #!/usr/bin/env node : pour execution
+
+const argument = process.argv[2];
+const number = parseInt(argument);
+if (isNaN(number)) {
+  console.log('Missing number of occurrences')
+} else
+for(let index = 0; index < number; index++) {
+  console.log('C is fun')
+}
+
