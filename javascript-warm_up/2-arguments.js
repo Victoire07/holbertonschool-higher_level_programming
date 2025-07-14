9685a4573b6df59b1e7ce0ba3006ee00e6d552ad
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 14:04:57 +0200

    1ère version de mon code pour la T2 mais rouge donc fausse

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// #!/usr/bin/env node : pour execution
+const args = process.argv;
+const userArgsCount = args.length - 2;
+
+if (args = 0)
+    const.log('No argument');
+else if (args = 1)
+    const.log('Argument found')
+else
+    const.log('Arguments found')
