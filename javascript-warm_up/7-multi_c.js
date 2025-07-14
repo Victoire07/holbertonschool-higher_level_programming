2302dd7af8ed481187a806f85a65a5bf5572d45e
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 17:08:54 +0200

    indentation

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-// #!/usr/bin/env node : pour execution
-
 const argument = process.argv[2];
 const number = parseInt(argument);
 if (isNaN(number)) {
@@ -8,5 +6,5 @@ if (isNaN(number)) {
 } else {
   for (let index = 0; index < number; index++) {
     console.log('C is fun');
-    }
+  }
 }
