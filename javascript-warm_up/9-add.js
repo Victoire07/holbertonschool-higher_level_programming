372dde4563758dd29a4f5bfab38b2274a4745693
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 18:34:15 +0200

    modif pour avoir le bon nom de fonction

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const premier_argument = process.argv[2];
 const deuxieme_argument = process.argv[3];
 const premier_nombre = parseInt(premier_argument);
 const deuxieme_nombre = parseInt(deuxieme_argument);
-function add(premier_nombre, deuxieme_nombre) {
-    return premier_nombre + deuxieme_nombre;
+function add(a, b) {
+  return a + b;
 }
 console.log(add(premier_nombre, deuxieme_nombre))
