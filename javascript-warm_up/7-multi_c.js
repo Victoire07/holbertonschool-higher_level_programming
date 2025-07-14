c0e8690fa1b88ad2479b3c7c3d19d188b26d1d61
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 17:00:12 +0200

    ajout des {}

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ const argument = process.argv[2];
 const number = parseInt(argument);
 if (isNaN(number)) {
   console.log('Missing number of occurrences')
-} else
-for(let index = 0; index < number; index++) {
-  console.log('C is fun')
+} else {
+  for(let index = 0; index < number; index++) {
+    console.log('C is fun')
 }
-
+}
