32c2670618470a9a7934982cea59d5b2b4884890
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 14:22:27 +0200

    modif style du code pour nore JS

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,10 +3,10 @@
 const args = process.argv;
 const userArgsCount = args.length - 2;
 
-if (userArgsCount === 0)
-    {console.log('No argument');}
-
-else if (userArgsCount === 1)
-    {console.log('Argument found');}
-else
-    {console.log('Arguments found');}
+if (userArgsCount === 0) {
+    console.log('No argument');
+} else if (userArgsCount === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
