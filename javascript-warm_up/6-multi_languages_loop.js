05c2719a6321ef3490252e1888c0a8fb481d653a
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 16:13:43 +0200

    remise de la shebang line demandé par le checker et non la nécéssaire à mon mac

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 // #!/usr/bin/env node : pour execution
 const lines = [ "C is fun", "Python is cool", "JavaScript is amazing" ];
 for(let index = 0; index < lines.length; index ++) {
