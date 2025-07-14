cf1baa3647d7d6f9916c18a8d9e2bed77b770ef1
Victoire07 <victoireboutin07@gmail.com>
2025-07-14 16:31:39 +0200

    supression d'un espace piur l'ibcrémentation

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // #!/usr/bin/env node : pour execution
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < lines.length; index ++) {
+for (let index = 0; index < lines.length; index++) {
   console.log(lines[index]);
 }
