32fb005224ae01e8f4e3dea729cc6c6cf4e81c0b
Victoire07 <victoireboutin07@gmail.com>
2025-07-15 12:59:14 +0200

    indnetation

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,11 +2,12 @@
 const arg = parseInt(process.argv[2]);
 function factorial (n) {
   if (n === 0) {
-  return (1);
+    return (1);
   } else if (n === 1) {
-  return (1);
+    return (1);
   }
   return n * factorial(n - 1);
+
 }
 if (isNaN(arg)) {
   console.log(1);
