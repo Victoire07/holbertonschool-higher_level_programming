02572d13246e5caa1cf1e3a8cc89e1335dd66dc6
Victoire07 <victoireboutin07@gmail.com>
2025-07-15 12:58:09 +0200

    ajout de la bonne indentation (2 au lieu de 4) et espace manquant

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const arg = parseInt(process.argv[2]);
-function factorial(n) {
+function factorial (n) {
   if (n === 0) {
-    return (1);
-    } else if (n === 1) {
-    return (1);
-    }
-    return n * factorial(n - 1);
+  return (1);
+  } else if (n === 1) {
+  return (1);
+  }
+  return n * factorial(n - 1);
 }
 if (isNaN(arg)) {
   console.log(1);
