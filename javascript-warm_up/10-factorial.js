f95691a98e08b45b1dc99d612514576d78b45dba
Victoire07 <victoireboutin07@gmail.com>
2025-07-15 12:22:04 +0200

    affiche 1 si l'argument n'est pas un nombre (Not a number)

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,9 +3,13 @@ const arg = parseInt(process.argv[2]);
 function factorial(n) {
   if (n === 0) {
     return (1);
-} else if (n === 1) {
+    } else if (n === 1) {
     return (1);
+    }
+    return n * factorial(n - 1);
 }
-return n * factorial(n - 1);
+if (isNaN(arg)) {
+  console.log(1);
+} else {
+  console.log(factorial(arg));
 }
-console.log(factorial(arg));
