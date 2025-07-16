fd35f97b0244d960f2b8edfcfdb88b665e11c806
Victoire07 <victoireboutin07@gmail.com>
2025-07-17 00:12:49 +0200

    Norme semistrandard

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -2,8 +2,8 @@ const movies = document.getElementById('list_movies');
 fetch('https://swapi-api.hbtn.io/api/films/?format=json')
   .then(response => response.json())
   .then(data => {
-  data.results.forEach(film => {
-    const li = document.createElement('li');
+    data.results.forEach(film => {
+      const li = document.createElement('li');
       li.textContent = film.title;
       movies.appendChild(li);
     });
