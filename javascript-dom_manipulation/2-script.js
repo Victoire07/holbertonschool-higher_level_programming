96eb92410882febb79ac8b63499672c2dee5a57d
Victoire07 <victoireboutin07@gmail.com>
2025-07-16 17:06:04 +0200

    ajout ;

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,4 +1,4 @@
-const red_header = document.querySelector('#red_header')
+const red_header = document.querySelector('#red_header');
 const header = document.querySelector('header');
 red_header.addEventListener('click',() => {
   header.classList.add('red');
