e567ed662b37474b92aa6a6061d1cb8bcb25eeac
Victoire07 <victoireboutin07@gmail.com>
2025-07-16 17:31:24 +0200

    Modif de toutes les erreurs avec 'semistrandard'

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,10 +1,11 @@
 const div = document.getElementById('toggle_header');
 const header = document.querySelector('header');
-div.addEventListener('click',() => {
+div.addEventListener('click', () => {
   if (header.classList.contains('red')) {
     header.classList.remove('red');
     header.classList.add('green');
   } else {
-  header.classList.remove('green');
-  header.classList.add('red'); }
+    header.classList.remove('green');
+    header.classList.add('red');
+  }
 });
