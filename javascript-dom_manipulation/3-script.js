7272c49aaaa4e6cd593a7c39e9529f8924aef7c9
Victoire07 <victoireboutin07@gmail.com>
2025-07-16 17:27:19 +0200

    Basculer la classe du header entre 'red' et 'green' au click

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -2,7 +2,9 @@ const div = document.getElementById('toggle_header');
 const header = document.querySelector('header');
 div.addEventListener('click',() => {
   if (header.classList.contains('red')) {
-    header.classList.remove('red')
-    header.classList.add('green')
-  }
+    header.classList.remove('red');
+    header.classList.add('green');
+  } else {
+  header.classList.remove('green');
+  header.classList.add('red'); }
 });
