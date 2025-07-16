8bac910cea6eae041682a0796e01d928af914407
Victoire07 <victoireboutin07@gmail.com>
2025-07-16 17:21:57 +0200

    gestion du cas si header est rouge

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,5 +1,8 @@
 const div = document.getElementById('toggle_header');
 const header = document.querySelector('header');
 div.addEventListener('click',() => {
-
+  if (header.classList.contains('red')) {
+    header.classList.remove('red')
+    header.classList.add('green')
+  }
 });
