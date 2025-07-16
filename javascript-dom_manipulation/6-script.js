d8b4ff446814ab370a571d56c00a2c536cfd85a8
Victoire07 <victoireboutin07@gmail.com>
2025-07-16 23:39:51 +0200

    Modification pour répondre à la norme semistandard

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -2,5 +2,5 @@ const character = document.getElementById('character');
 fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
   .then(response => response.json())
   .then(data => {
-    character.textContent = data.name
+    character.textContent = data.name;
   });
