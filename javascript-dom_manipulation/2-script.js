a878e28ca035b2af8599ef1304f217239e06289d
Victoire07 <victoireboutin07@gmail.com>
2025-07-16 13:24:05 +0200

    Ajouter la classe CSS 'red' au header au clic sur #red_header

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+const red_header = document.querySelector('#red_header')
+const header = document.querySelector('header');
+red_header.addEventListener('click',() => {
+  header.classList.add('red');
+});
