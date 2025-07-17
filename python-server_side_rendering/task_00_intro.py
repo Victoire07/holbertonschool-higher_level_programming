2ba134777751bf89bc9f55e8906bbd5a549a2e9c
Victoire07 <victoireboutin07@gmail.com>
2025-07-17 14:34:03 +0200

    ajout d'un return manquant

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,6 +1,7 @@
 def generate_invitations(template, attendees):
     if not isinstance(template, str):
         print ("Error: template must be a string!")
+        return
     if not isinstance(attendees, list) or not all(isinstance(person, dict) for person in attendees):
-        print("Error: Attendees must be a list of dictionaries.")
+        print("Error: attendees must be a list of dictionaries!")
         return
