66f3e26ec000f815c18ffe333a6af53b1d37ac40
Victoire07 <victoireboutin07@gmail.com>
2025-07-17 14:56:09 +0200

    Liste des champs à remplacer : ajout

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -6,9 +6,13 @@ def generate_invitations(template, attendees):
     if not isinstance(attendees, list) or not all(isinstance(person, dict) for person in attendees):
         print("Error: attendees must be a list of dictionaries!")
         return
+    # vérifie si le template est vide 
     if template.strip() == "":
         print ("The model is empty, no output file is generated")
         return
+    # vérifie si la liste attendees est vide
     if not attendees:
         print ("No data is supplied and no output file is generated")
         return
+    
+    replace_fields = ["name", "event_title", "event_date", "event_location"]
