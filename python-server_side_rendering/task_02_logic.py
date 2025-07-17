5d0acf0f14bccbe78a7d53b76d4beac10746a85e
Victoire07 <victoireboutin07@gmail.com>
2025-07-17 23:30:53 +0200

    lecture du fichier items.json et envoi dynamique des items au template

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -5,4 +5,16 @@ app = Flask(__name__)
 
 @app.route('/items')
 def items():
-    return render_template('items.html', items=[])
+    try:
+        with open('items.json') as f:
+            data = json.load(f)  # transforme le JSON en dictionnaire Python
+
+        # Récupère la liste des items
+        items_list = data.get('items', [])  # si 'items' n'existe pas dcp retourne liste vide !
+
+    except Exception as erreur:
+        print(f"Erreur lors de la lecture du fichier JSON : {erreur}")
+        items_list = []
+
+    # Passation de la liste au template
+    return render_template('items.html', items=items_list)
