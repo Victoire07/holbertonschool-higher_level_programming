d78ee796d0109459a8e8daeb9001437e20ba305b
Victoire07 <victoireboutin07@gmail.com>
2025-07-17 23:49:49 +0200

    implémentation des routes qui avaient été définis dans la T1

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -3,6 +3,18 @@ import json
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
 @app.route('/items')
 def items():
     try:
@@ -18,3 +30,6 @@ def items():
 
     # Passation de la liste au template
     return render_template('items.html', items=items_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
